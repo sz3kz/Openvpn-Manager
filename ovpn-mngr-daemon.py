be86b64606d012cd28cb186ca280546c295ab36d
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-02 17:05:38 +0200

    Implement standardized and verbose print messages.

--- ovpn-mngr-daemon.py
+++ ovpn-mngr-daemon.py
@@ -16,124 +16,145 @@ VPN_LINK=f"{ROOT_MNGR_DIR}/current"
 def respond(message):
     with open(f"{OUTPUT_PIPE}", 'w') as output_pipe:
         output_pipe.write(f"{message}")
+    print(f"    > \'{message}\'")
 
 def receive():
     with open(f"{INPUT_PIPE}", 'r') as input_pipe:
         response = input_pipe.read().strip()
+    print(f"    < \'{response}\'")
     return response
 
 def terminate():
+    print("#### Client requested termination of daemon.")
     if connection_active:
         process.kill()
-    respond("TERMINATED")
+        print(f"Killed still active openvpn connection.")
     for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
         os.remove(f"{pipe}")
-        print(f"Pipe {pipe} removed.")
-    print("Terminated")
+        print(f"Removed pipe: \'{pipe}\'.")
+    print("Daemon terminated.")
+    respond("TERMINATED")
     sys.exit(0)
 
 def status():
+    print("#### Client queried status of connection.")
     if connection_active:
-        print("Connection is active")
+        print("Connection is active.")
         respond(f"CONNECTED")
     else:
-        print("Connection is not active")
+        print("Connection is not active.")
         respond(f"DISCONNECTED")
 
 def available():
+    print("#### Client requested a listing of available vpn files.")
     files = os.listdir(f"{VPN_DIR}")
-    print(f"{len(files)} files available.")
+    print(f"Files available: {len(files)}.")
     respond(f"{len(files)}")
+    print(f"Listing files:")
     for file in files:
-        print(f"{file}")
+        print(f" - \'{file}\'")
         response = receive()
         if not response == "CONTINUE":
-            return None
+            return
         respond(f"{file}")
     print("Listing done.")
 
 def upload():
+    print("#### Client requested a vpn file upload.")
     respond("PATH?")
-    path = receive()
-    if not os.path.exists(path):
-        print(f"User supplied ovpn file: \'{path}\' does not exist.")
+    src_path = receive()
+    if not os.path.exists(src_path):
+        print(f"Non-existant source file denoted: \'{src_path}\'.")
         respond("ERROR:INVALIDFILE")
         return
+    print("Source file : \'{src_path}\'")
     respond("NEWNAME?")
-    newpath = receive()
-    newname = os.path.basename(newpath)
-    if os.path.exists(f"{VPN_DIR}/{newname}"):
-        print("User supplied ovpn file name already exists.")
+    dst_path_user = receive()
+    dst_name = os.path.basename(dst_path_user)
+    dst_path = f"{VPN_DIR}/{dst_name}"
+    if os.path.exists(f"{dst_path}"):
+        print("Already existant vpn file denoted: \'{dst_path}\'.")
         respond("ERROR:FILEEXISTS")
         return
-    shutil.copy(f"{path}",f"{VPN_DIR}/{newname}")
+    print("Target file : \'{dst_path}\'.")
+    shutil.copy(f"{path}",f"{dst_path}")
+    print("Copied source file to target file.")
     respond("SUCCESS")
 
 def current():
+    print("#### Client queried for currently selected vpn file.")
     if not os.path.exists(f"{VPN_LINK}"):
         respond("ERROR:NOFILESELECTED")
         return
     current_path = os.readlink(f"{VPN_LINK}")
     current_file = os.path.basename(f"{current_path}")
+    print(f"Currently selected file: \'{current_file}\'.")
     respond(f"{current_file}")
 
 def select():
+    print("#### Client requested to change the currently selected vpn file.")
     respond("NAME?")
-    name = os.path.basename(f"{receive()}")
-    if not os.path.exists(f"{VPN_DIR}/{name}"):
-        print("Selected file: \'{VPN_DIR}/{name}\' does not exist.")
+    path_user = receive()
+    name = os.path.basename(f"{path_user}")
+    path = f"{VPN_DIR}/{name}"
+    if not os.path.exists(f"{path}"):
+        print("Nonexist Selected file: \'{VPN_DIR}/{name}\' does not exist.")
         respond("ERROR:FILEDOESNOTEXIST")
-    print("Creating temporary link.")
+    print(f"Valid path given: \'{path}\'.")
     os.symlink(f"{VPN_DIR}/{name}", f"{ROOT_MNGR_DIR}/temporarylink")
     os.rename(f"{ROOT_MNGR_DIR}/temporarylink", f"{VPN_LINK}")
-    print("Symbolic link changed.")
+    print(f"Symbolic link changed: \'{VPN_LINK}\' -> \'{os.readlink(VPN_LINK)}\'.")
     respond("SUCCESS")
 
 def connect():
     global process
     global connection_active
+    print("#### Client requested to activate vpn.")
     if connection_active:
-        print("Openvpn daemon already started.")
+        print("Openvpn daemon is already active.")
         respond("ERROR:CONNECTED")
         return 
+    print("Activating vpn daemon.")
     process = subprocess.Popen(["openvpn", f"{VPN_LINK}"])
     connection_active = True
+    print("Daemon activated.")
     respond("SUCCESS")
 
 def disconnect():
     global process
     global connection_active
+    print("#### Client requested to deactivate vpn.")
     if not connection_active:
-        print("Openvpn daemon is not running.")
+        print("Openvpn daemon is not active.")
         respond("ERROR:DISCONNECTED")
         return
+    print("Deactivating vpn daemon.")
     process.kill()
     connection_active = False
+    print("Daemon deactivated.")
     respond("SUCCESS")
 
 
 if os.geteuid() != 0:
-    print(f"Insufficient privileges.")
+    print(f"Insufficient privileges: \'{os.geteuid()}\'.")
     print(f"{sys.argv[0]} must be ran with root privileges!")
     sys.exit(1)
-print("Sufficent privileges.")
+print(f"Sufficient privileges: \'{os.geteuid()}\'.")
 
 for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
     if os.path.exists(f"{pipe}"):
-        print(f"\'{pipe}\' already exists.")
+        print(f"Pipe already exists: \'{pipe}\'.")
         os.remove(f"{pipe}")
-        print(f"\'{pipe}\' deleted.")
+        print(f"Pipe deleted: \'{pipe}\'.")
     os.mkfifo(f"{pipe}")
-    print(f"\'{pipe}\' created.")
+    print(f"Pipe created: \'{pipe}\'.")
 
 connection_active = False
 process = None
 while True:
     print("Command loop started.")
-    print(f"Process: {process}")
-    print(f"connection_active: {connection_active}")
     command = receive()
-    print(f"Command: \'{command}\'")
+    print(f"Command received: \'{command}\'.")
     match command:
         case "TERMINATE":
             terminate()
@@ -152,5 +173,5 @@ while True:
         case "DISCONNECT":
             disconnect()
         case _:
-            print(f"ERROR: command \'{command}\' not supported.")
+            print(f"Command \'{command}\' not supported.")
             respond("ERROR:INVALIDCOMMAND")
