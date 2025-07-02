942616af6047f74fa09e79536bb16907d794d321
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-02 19:36:27 +0200

    Breathe life into the log file via termcolor.

--- ovpn-mngr-daemon.py
+++ ovpn-mngr-daemon.py
@@ -4,6 +4,7 @@ import sys
 import shutil
 import subprocess
 import logging
+from termcolor import colored
 
 ROOT_PIPE_DIR="/var/run"
 INPUT_PIPE=f"{ROOT_PIPE_DIR}/ovpn-mngr-server.pipe"
@@ -16,165 +17,176 @@ LOG_FILE=f"{ROOT_MNGR_DIR}/log.txt"
 
 
 
+def failure(message):
+    return colored(f'{message}','red')
+
+def success(message):
+    return colored(f'{message}', 'green')
+
+def inform(message):
+    return colored(f'{message}', 'yellow')
+
 def respond(message):
     with open(f"{OUTPUT_PIPE}", 'w') as output_pipe:
         output_pipe.write(f"{message}")
-    logger.info(f"    > \'{message}\'")
+    logger.info(inform(f"    > \'{message}\'"))
 
 def receive():
     with open(f"{INPUT_PIPE}", 'r') as input_pipe:
         response = input_pipe.read().strip()
-    logger.info(f"    < \'{response}\'")
+    logger.info(inform(f"    < \'{response}\'"))
     return response
 
 def setup_pipes():
     for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
         if os.path.exists(f"{pipe}"):
-            logger.info(f"Pipe already exists: \'{pipe}\'.")
+            logger.info(inform(f"Pipe already exists: \'{pipe}\'."))
             os.remove(f"{pipe}")
-            logger.info(f"Pipe deleted: \'{pipe}\'.")
+            logger.info(inform(f"Pipe deleted: \'{pipe}\'."))
         os.mkfifo(f"{pipe}")
-        logger.info(f"Pipe created: \'{pipe}\'.")
+        logger.info(inform(f"Pipe created: \'{pipe}\'."))
+    logger.info(success("Pipes set up."))
 
 def check_root_privileges():
     if os.geteuid() != 0:
-        logger.error(f"Insufficient privileges: \'{os.geteuid()}\'.")
-        logger.error(f"{sys.argv[0]} must be ran with root privileges!")
+        logger.error(failure(f"Insufficient privileges: \'{os.geteuid()}\'."))
+        logger.error(failure(f"{sys.argv[0]} must be ran with root privileges!"))
         sys.exit(1)
-    logger.info(f"Sufficient privileges: \'{os.geteuid()}\'.")
+    logger.info(success(f"Sufficient privileges: \'{os.geteuid()}\'."))
 
 
 def terminate():
-    logger.info("#### Client requested termination of daemon.")
+    logger.info(inform("#### Client requested termination of daemon."))
     if connection_active:
         process.kill()
-        logger.info(f"Killed still active openvpn connection.")
+        logger.info(inform(f"Killed still active openvpn connection."))
     for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
         os.remove(f"{pipe}")
-        logger.info(f"Removed pipe: \'{pipe}\'.")
-    logger.info("Daemon terminated.")
+        logger.info(inform(f"Removed pipe: \'{pipe}\'."))
+    logger.info(inform("Daemon terminated."))
     respond("TERMINATED")
     sys.exit(0)
 
 def status():
-    logger.info("#### Client queried status of connection.")
+    logger.info(inform("#### Client queried status of connection."))
     if connection_active:
-        logger.info("Connection is active.")
+        logger.info(inform("Connection is active."))
         respond("CONNECTED")
     else:
-        logger.info("Connection is not active.")
+        logger.info(inform("Connection is not active."))
         respond("DISCONNECTED")
+    logger.info(success("Status query answered."))
 
 def available():
-    logger.info("#### Client requested a listing of available vpn files.")
+    logger.info(inform("#### Client requested a listing of available vpn files."))
     files = os.listdir(f"{VPN_DIR}")
-    logger.info(f"Files available: {len(files)}.")
+    logger.info(inform(f"Files available: {len(files)}."))
     respond(f"{len(files)}")
-    logger.info("Listing files:")
+    logger.info(inform("Listing files:"))
     for file in files:
-        logger.info(f" - \'{file}\'")
+        logger.info(inform(f" - \'{file}\'"))
         response = receive()
         if not response == "CONTINUE":
             return
         respond(f"{file}")
-    logger.info("Listing done.")
+    logger.info(success("Listing done."))
 
 def upload():
-    logger.info("#### Client requested a vpn file upload.")
+    logger.info(inform("#### Client requested a vpn file upload."))
     respond("PATH?")
     src_path = receive()
     if not os.path.exists(src_path):
-        logger.error(f"Non-existant source file denoted: \'{src_path}\'.")
+        logger.error(failure(f"Non-existant source file denoted: \'{src_path}\'."))
         respond("ERROR:INVALIDFILE")
         return
     if not os.path.isfile(src_path):
-        logger.error(f"Non-file path denoted: \'{src_path}\'")
+        logger.error(failure(f"Non-file path denoted: \'{src_path}\'"))
         respond("ERROR:NOTAFILE")
         return
-    logger.info(f"Source file : \'{src_path}\'")
+    logger.info(inform(f"Source file : \'{src_path}\'"))
     respond("NEWNAME?")
     dst_path_user = receive()
     dst_name = os.path.basename(dst_path_user)
     dst_path = f"{VPN_DIR}/{dst_name}"
     if os.path.exists(f"{dst_path}"):
-        logger.info(f"Already existant vpn file denoted: \'{dst_path}\'.")
+        logger.info(failure(f"Already existant vpn file denoted: \'{dst_path}\'."))
         respond("ERROR:FILEEXISTS")
         return
-    logger.info(f"Target file : \'{dst_path}\'.")
+    logger.info(inform(f"Target file : \'{dst_path}\'."))
     shutil.copy(f"{src_path}",f"{dst_path}")
-    logger.info("Copied source file to target file.")
+    logger.info(success("Copied source file to target file."))
     respond("SUCCESS")
 
 def delete():
-    logger.info("#### Client requested a vpn file deletion.")
+    logger.info(inform("#### Client requested a vpn file deletion."))
     respond("NAME?")
     target_path_user = receive()
     target_name = os.path.basename(target_path_user)
     target_path = f"{VPN_DIR}/{target_name}"
     if not os.path.exists(f"{target_path}"):
-        logger.error("Non-existant vpn file denoted: \'{target_path}\'.")
+        logger.error(failure("Non-existant vpn file denoted: \'{target_path}\'."))
         respond("ERROR:FILEDOESNOTEXIST")
         return
-    logger.info("File to be deleted: \'{target_path}\'.")
+    logger.info(inform("File to be deleted: \'{target_path}\'."))
     os.remove(f"{target_path}")
-    logger.info("File deleted.")
+    logger.info(success("File deleted."))
     respond("SUCCESS")
 
 
 
 def current():
-    logger.info("#### Client queried for currently selected vpn file.")
+    logger.info(inform("#### Client queried for currently selected vpn file."))
     if not os.path.exists(f"{VPN_LINK}"):       # this also takes care of broken links
-        logger.error(f"Symbolic link does not exist: \'{VPN_LINK}\'.")
+        logger.error(failure(f"Symbolic link does not exist: \'{VPN_LINK}\'."))
         respond("ERROR:NOFILESELECTED")
         return
     current_path = os.readlink(f"{VPN_LINK}")
     current_file = os.path.basename(f"{current_path}")
-    logger.info(f"Currently selected file: \'{current_file}\'.")
+    logger.info(inform(f"Currently selected file: \'{current_file}\'."))
     respond(f"{current_file}")
 
 def select():
-    logger.info("#### Client requested to change the currently selected vpn file.")
+    logger.info(inform("#### Client requested to change the currently selected vpn file."))
     respond("NAME?")
     path_user = receive()
     name = os.path.basename(f"{path_user}")
     path = f"{VPN_DIR}/{name}"
     if not os.path.exists(f"{path}"):
-        logger.error("Nonexist Selected file: \'{VPN_DIR}/{name}\' does not exist.")
+        logger.error(failure("Nonexist Selected file: \'{VPN_DIR}/{name}\' does not exist."))
         respond("ERROR:FILEDOESNOTEXIST")
         return
-    logger.info(f"Valid path given: \'{path}\'.")
+    logger.info(inform(f"Valid path given: \'{path}\'."))
     os.symlink(f"{VPN_DIR}/{name}", f"{ROOT_MNGR_DIR}/temporarylink")
     os.rename(f"{ROOT_MNGR_DIR}/temporarylink", f"{VPN_LINK}")
-    logger.info(f"Symbolic link changed: \'{VPN_LINK}\' -> \'{os.readlink(VPN_LINK)}\'.")
+    logger.info(success(f"Symbolic link changed: \'{VPN_LINK}\' -> \'{os.readlink(VPN_LINK)}\'."))
     respond("SUCCESS")
 
 def connect():
     global process
     global connection_active
-    logger.info("#### Client requested to activate vpn.")
+    logger.info(inform("#### Client requested to activate vpn."))
     if connection_active:
-        logger.error("Openvpn daemon is already active.")
+        logger.error(failure("Openvpn daemon is already active."))
         respond("ERROR:CONNECTED")
         return
-    logger.info("Activating vpn daemon.")
+    logger.info(inform("Activating vpn daemon."))
     process = subprocess.Popen(["openvpn", f"{VPN_LINK}"])
     connection_active = True
-    logger.info("Daemon activated.")
+    logger.info(success("Daemon activated."))
     respond("SUCCESS")
 
 def disconnect():
     global process
     global connection_active
-    logger.info("#### Client requested to deactivate vpn.")
+    logger.info(inform("#### Client requested to deactivate vpn."))
     if not connection_active:
-        logger.error("Openvpn daemon is not active.")
+        logger.error(failure("Openvpn daemon is not active."))
         respond("ERROR:DISCONNECTED")
         return
-    logger.info("Deactivating vpn daemon.")
+    logger.info(inform("Deactivating vpn daemon."))
     process.kill()
     connection_active = False
-    logger.info("Daemon deactivated.")
+    logger.info(success("Daemon deactivated."))
     respond("SUCCESS")
 
 def main():
@@ -191,9 +203,9 @@ def main():
     connection_active = False
     process = None
     while True:
-        logger.info("Command loop started.")
+        logger.info(inform("Command loop started."))
         command = receive()
-        logger.info(f"Command received: \'{command}\'.")
+        logger.info(inform(f"Command received: \'{command}\'."))
         match command:
             case "TERMINATE":
                 terminate()
@@ -214,7 +226,7 @@ def main():
             case "DISCONNECT":
                 disconnect()
             case _:
-                logger.error(f"Command \'{command}\' not supported.")
+                logger.error(failure(f"Command \'{command}\' not supported."))
                 respond("ERROR:INVALIDCOMMAND")
 
 if __name__ == '__main__':
