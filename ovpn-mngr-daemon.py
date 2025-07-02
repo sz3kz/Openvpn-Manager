62a8e714e454b67b51e7b7a45f4328ded43a4267
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-02 10:37:09 +0200

    Add privilege check to ovpn-mngr-daemon.py

---
+++ ovpn-mngr-daemon.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+if os.geteuid() != 0:
+    print(f"Insufficient privileges.")
+    print(f"{sys.argv[0]} must be ran with root privileges!")
+    sys.exit(1)
+print("Sufficent privileges.")
