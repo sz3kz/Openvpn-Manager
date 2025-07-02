d4a14ef805d6301a4d386b87585e15d0ff42a1fc
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-02 10:52:56 +0200

    Add pipe creation functionality to ovpn-mngr-daemon.py .

--- ovpn-mngr-daemon.py
+++ ovpn-mngr-daemon.py
@@ -2,8 +2,21 @@
 import os
 import sys
 
+ROOT_PIPE_DIR="/var/run"
+INPUT_PIPE=f"{ROOT_PIPE_DIR}/ovpn-mngr-server.pipe"
+OUTPUT_PIPE=f"{ROOT_PIPE_DIR}/ovpn-mngr-client.pipe"
+
+
 if os.geteuid() != 0:
     print(f"Insufficient privileges.")
     print(f"{sys.argv[0]} must be ran with root privileges!")
     sys.exit(1)
 print("Sufficent privileges.")
+
+for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
+    if os.path.exists(f"{pipe}"):
+        print(f"\'{pipe}\' already exists.")
+        os.remove(f"{pipe}")
+        print(f"\'{pipe}\' deleted.")
+    os.mkfifo(f"{pipe}")
+    print(f"\'{pipe}\' created.")
