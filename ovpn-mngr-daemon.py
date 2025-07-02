8689ba81d2382f51537d675687bc830d28426671
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-02 23:47:58 +0200

    Create google-style docstrings for functions.

--- ovpn-mngr-daemon.py
+++ ovpn-mngr-daemon.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+'''
+OpenVPN management daemon for ease of handling of simple openvpn connections.
+'''
+
 import os
 import sys
 import shutil
 import subprocess
 import logging
-from termcolor import colored
 import stat
+
+from termcolor import colored
 
 ROOT_PIPE_DIR="/var/run"
 INPUT_PIPE=f"{ROOT_PIPE_DIR}/ovpn-mngr-server.pipe"
@@ -19,26 +24,83 @@ LOG_FILE=f"{ROOT_MNGR_DIR}/log.txt"
 
 
 def failure(message):
+    '''
+    Returns red color text.
+
+    Args:
+        message (str): message to be colored
+
+    Returns:
+        (str): colored message
+    '''
     return colored(f'{message}','red')
 
 def success(message):
+    '''
+    Returns green color text.
+
+    Args:
+        message (str): message to be colored
+
+    Returns:
+        (str): colored message
+    '''
     return colored(f'{message}', 'green')
 
 def inform(message):
+    '''
+    Returns yellow color text.
+
+    Args:
+        message (str): message to be colored
+
+    Returns:
+        (str): colored message
+    '''
     return colored(f'{message}', 'yellow')
 
 def respond(message):
+    '''
+    Writes message to the pipe the client is reading from.
+
+    Args:
+        message (str): message to write to pipe
+
+    Returns:
+        None
+    '''
     with open(f"{OUTPUT_PIPE}", 'w') as output_pipe:
         output_pipe.write(f"{message}")
     logger.info(inform(f"    > \'{message}\'"))
 
 def receive():
+    '''
+    Reads message from the pipe the client is writing to.
+
+    Args:
+        message (str): message to write to pipe
+
+    Returns:
+        None
+    '''
     with open(f"{INPUT_PIPE}", 'r') as input_pipe:
         response = input_pipe.read().strip()
     logger.info(inform(f"    < \'{response}\'"))
     return response
 
 def setup_pipes():
+    '''
+    Ensures the pipes are correctly set up. Checks if pipes exist and if they do, 
+    deletes them (to be deterministic). Then creates the pipes with appropriate
+    permissions.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    '''
     for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
         if os.path.exists(f"{pipe}"):
             logger.info(inform(f"Pipe already exists: \'{pipe}\'."))
@@ -47,10 +109,19 @@ def setup_pipes():
         os.mkfifo(f"{pipe}")
         logger.info(inform(f"Pipe created: \'{pipe}\'."))
         os.chmod(pipe, stat.S_IREAD | stat.S_IWRITE)
-        logger.info(inform(f"Pipe permissions modified."))
+        logger.info(inform("Pipe permissions modified."))
     logger.info(success("Pipes set up."))
 
 def check_root_privileges():
+    '''
+    Checks if daemon is ran with appropriate (root!) privileges. Exits if not root.
+
+    Args:
+        None
+
+    Returns:
+        None
+    '''
     if os.geteuid() != 0:
         logger.error(failure(f"Insufficient privileges: \'{os.geteuid()}\'."))
         logger.error(failure(f"{sys.argv[0]} must be ran with root privileges."))
@@ -59,11 +130,23 @@ def check_root_privileges():
 
 
 def terminate():
+    '''
+    Handles the 'TERMINATE' command. Kills active openvpn process. Deletes
+    the pipes.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'TERMINATED'
+    '''
     logger.info(inform("#### Client requested termination of daemon."))
     if connection_active:
         process.kill()
-        logger.info(inform(f"Killed still active openvpn connection."))
-    respond("TERMINATED")
+        logger.info(inform("Killed still active openvpn connection."))
     for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
         os.remove(f"{pipe}")
         logger.info(inform(f"Removed pipe: \'{pipe}\'."))
@@ -71,6 +154,19 @@ def terminate():
     sys.exit(0)
 
 def status():
+    '''
+    Handles the 'STATUS' command. Determines whether the openvpn process is active.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'CONNECTED' : process is running
+        'DISCONNECTED' : process is not running / was killed 
+    '''
     logger.info(inform("#### Client queried status of connection."))
     if connection_active:
         logger.info(inform("Connection is active."))
@@ -81,6 +177,20 @@ def status():
     logger.info(success("Status query answered."))
 
 def available():
+    '''
+    Handles the 'AVAILABLE' command. Sends the count of files inside the VPN_DIR 
+    directory. Transmits every file name one-by-one, waiting for 'CONTINUE' responses.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        [count of files]
+        [name of file]
+    '''
     logger.info(inform("#### Client requested a listing of available vpn files."))
     files = os.listdir(f"{VPN_DIR}")
     logger.info(inform(f"Files available: {len(files)}."))
@@ -95,6 +205,26 @@ def available():
     logger.info(success("Listing done."))
 
 def upload():
+    '''
+    Handles the 'UPLOAD' command. Queries user for path of file and it's preferred
+    basename via 'PATH?' and 'NEWNAME?'. Copies path to VPN_DIR with new basename.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'PATH?': query for path of vpn file
+        'NEWNAME?': query for preferred basename of vpn file
+        'SUCCESS': upload was successful
+
+    Errors:
+        'ERROR:INVALIDFILE': given path does not exist
+        'ERROR:NOTAFILE': given path does not point to a file
+        'ERROR:FILEEXISTS: given preferred basename already exists in VPN_DIR'
+    '''
     logger.info(inform("#### Client requested a vpn file upload."))
     respond("PATH?")
     src_path = receive()
@@ -121,6 +251,23 @@ def upload():
     respond("SUCCESS")
 
 def delete():
+    '''
+    Handles the DELETE command. Deletes vpn file name in VPN_DIR denoted by the user.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'NAME?': queries for name of file in VPN_DIR
+        'SUCCESS': deletion was successful
+
+    Errors:
+        'ERROR:FILEDOESNOTEXIST': denoted file does not exist.
+
+    '''
     logger.info(inform("#### Client requested a vpn file deletion."))
     respond("NAME?")
     target_path_user = receive()
@@ -138,6 +285,23 @@ def delete():
 
 
 def current():
+    '''
+    Handles the 'CURRENT' command. Sends the name of VPN_DIR file currently selected
+    for connecting, which can be accessed through a deterministic symbolic link to it.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        [currently selected file name]
+
+    Errors:
+        'ERROR:NOFILESELECTED': symbolic link either does not exist or is broken.
+
+    '''
     logger.info(inform("#### Client queried for currently selected vpn file."))
     if not os.path.exists(f"{VPN_LINK}"):       # this also takes care of broken links
         logger.error(failure(f"Symbolic link does not exist: \'{VPN_LINK}\'."))
@@ -149,6 +313,23 @@ def current():
     respond(f"{current_file}")
 
 def select():
+    '''
+    Handles the 'SELECT' command. Changes / creates the symbolic link pointing to the vpn file
+    to be used when connecting.
+    
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'NAME?': name of vpn file for the symbolic link to point to.
+        'SUCCESS': symbolic link creation / relinking was successful
+
+    Errors:
+        'ERROR:FILEDOESNOTEXIST': file of denoted name does not exist in VPN_DIR
+    '''
     logger.info(inform("#### Client requested to change the currently selected vpn file."))
     respond("NAME?")
     path_user = receive()
@@ -165,6 +346,22 @@ def select():
     respond("SUCCESS")
 
 def connect():
+    '''
+    Handles the 'CONNECT' command. Executes the openvpn process with the vpn file pointed to by 
+    the symbolic link.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'SUCCESS': establishment of connection was successful.
+
+    Errors:
+        'ERROR:CONNECTED': the connection was already established.
+    '''
     global process
     global connection_active
     logger.info(inform("#### Client requested to activate vpn."))
@@ -179,6 +376,21 @@ def connect():
     respond("SUCCESS")
 
 def disconnect():
+    '''
+    Handles the DISCONNECT command. Kills the opevpn process.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Responses:
+        'SUCCESS': the connection was severed successfully.
+
+    Errors:
+        'ERROR:DISCONNECTED': connection was not established before.
+    '''
     global process
     global connection_active
     logger.info(inform("#### Client requested to deactivate vpn."))
@@ -193,11 +405,16 @@ def disconnect():
     respond("SUCCESS")
 
 def main():
-    global logger 
+    '''
+    The brain of the script.
+    '''
+    global logger
     global connection_active
     global process
+
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename=f'{LOG_FILE}', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename=f'{LOG_FILE}', encoding='utf-8', level=logging.DEBUG,
+                        format='%(asctime)s %(message)s')
 
     check_root_privileges()
 
