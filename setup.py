b8fe89689558e0305b9346fcd19ac6bfe66cf164
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-02 10:13:42 +0200

    Add permission check to setup.py .

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+ROOT_DIRECTORY="/root/.openvpn-management/"
+
+if os.geteuid() != 0:
+    print(f"Insufficient privileges.")
+    print(f"{sys.argv[0]} must be ran with root privileges!")
+    sys.exit(1)
+print("Sufficent privileges.")
+
