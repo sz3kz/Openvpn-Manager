7e2fb9ce09a8847914f00e4e36f414566a4e0a11
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-07-04 13:32:08 +0200

    Add color to result output.

--- ovpn-mngr.py
+++ ovpn-mngr.py
@@ -3,10 +3,23 @@
 import sys
 import os
 
+from termcolor import colored
+
 ROOT_PIPE_DIR="/var/run"
 OUTPUT_PIPE=f"{ROOT_PIPE_DIR}/ovpn-mngr-server.pipe"
 INPUT_PIPE=f"{ROOT_PIPE_DIR}/ovpn-mngr-client.pipe"
 
+
+def failure(message):
+    return colored(f'{message}','red')
+
+
+def success(message):
+    return colored(f'{message}','green')
+
+
+def inform(message):
+    return colored(f'{message}','yellow')
 
 
 def send(message):
@@ -22,154 +35,148 @@ def receive():
 
 def check_root_privileges():
     if os.geteuid() != 0:
-        print("Run client with root privileges.")
+        print(failure("Insufficient (non-root!) privileges detected."))
         sys.exit(1)
 
 
 def check_pipes():
     for pipe in [INPUT_PIPE, OUTPUT_PIPE]:
         if not os.path.exists(f"{pipe}"):
-            print(f"Error: Missing pipe: \'{pipe}\'.")
+            print(failure(f"Missing pipe: \'{pipe}\'."))
             sys.exit(1)
 
 
 def terminate():
     if len(sys.argv) != 2:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
-    print("Valid command format.")
     send('TERMINATE')
-    print("Terminate sent")
     response = receive()
     if response == "TERMINATED":
-        print("Command succeeded!")
+        print(success("Daemon terminated."))
         return
-    print("ERROR: Command failed.")
+    print(failure("Command failed."))
 
 
 def status():
     if len(sys.argv) != 2:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
-    print("Valid command format.")
     send('STATUS')
-    print("STATUS sent")
     response = receive()
     if response == "CONNECTED":
-        print("Connection live.")
+        print(inform("Connection: On."))
     elif response == "DISCONNECTED":
-        print("No connection.")
+        print(inform("Connection: Off."))
     else:
-        print("ERROR: Command failed.")
+        print(failure("Command failed."))
 
 def available():
     if len(sys.argv) != 2:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
-    print("Valid command format.")
     send('AVAILABLE')
     file_amount = int(receive())
-    print(f"Listing files({file_amount}):")
+    print(inform(f"Listing files({file_amount}):"))
     for _ in range (0, file_amount):
         send("CONTINUE")
         file = receive()
-        print(f" - {file}")
-    print("Command succeeded.")
+        print(inform(f" - {file}"))
+    print(success("Listing done."))
 
 def upload():
     if len(sys.argv) != 4:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
     path = sys.argv[2]
     newname = sys.argv[3]
-    print("Valid command format.")
     send('UPLOAD')
     response = receive()
     if not response == 'PATH?':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
     send(f"{path}")
     response = receive()
     if not response == 'NEWNAME?':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
     send(f"{newname}")
     response = receive()
     if response != 'SUCCESS':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
-    print("Command succeeded.")
+    print(success("File uploaded."))
 
 def delete():
     if len(sys.argv) != 3:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
     file = sys.argv[2]
     send('DELETE')
     response = receive()
     if not response == 'NAME?':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
     send(f'{file}')
     response = receive()
     if not response == 'SUCCESS':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
-    print("Command succeeded.")
+    print(success("File deleted."))
 
 
 def current():
     if len(sys.argv) != 2:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
     send('CURRENT')
     file = receive()
     if file == "ERROR:NOFILESELECTED":
-        print("No file has been selected.")
-    print(f"Current selected file: {file}")
+        print(inform("No file has been selected."))
+        return
+    print(inform(f"Current selected file: \'{file}\'"))
 
 
 def select():
     if len(sys.argv) != 3:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
     file = sys.argv[2]
     send('SELECT')
     response = receive()
     if not response == 'NAME?':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
     send(f"{file}")
     response = receive()
     if not response == 'SUCCESS':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
-    print("Command succeeded.")
+    print(success("File selected."))
 
 
 def connect():
     if not len(sys.argv) == 2:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
     send('CONNECT')
     response = receive()
     if not response == 'SUCCESS':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
-    print("Command succeeded.")
+    print(success("Connection established."))
 
 
 def disconnect():
     if not len(sys.argv) == 2:
-        print("Invalid command format.")
+        print(failure("Invalid command format."))
         sys.exit(1)
     send('DISCONNECT')
     response = receive()
     if not response == 'SUCCESS':
-        print("Command failed.")
+        print(failure("Command failed."))
         sys.exit(1)
-    print("Command succeeded.")
-
+    print(success("Connection severed."))
 
 
 
@@ -177,7 +184,7 @@ def main():
     # check_root_privileges()
     check_pipes()
     if len(sys.argv) == 1:
-        print("No command found!")
+        print(failure("No command found!"))
         sys.exit(1)
     command = sys.argv[1]
     match command:
@@ -200,7 +207,7 @@ def main():
         case 'disconnect':
             disconnect()
         case _:
-            print(f"Error: unrecognized command: \'{command}\'")
+            print(failure(f"Unrecognized command: \'{command}\'"))
 
 
 if __name__ == '__main__':
